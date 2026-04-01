69ccdfe30c6945ae9013b58ae3f599c0a9a7017e
krishna <krishna_a@ce.iitr.ac.in>
2026-04-01 15:38:15 +0530

    Added Location Controller

---
+++ src/main/java/com/example/Uber/controllers/LocationController.java
@@ -0,0 +1,36 @@
+package com.example.Uber.controllers;
+
+import com.example.Uber.dtos.DriverLocationDTO;
+import com.example.Uber.dtos.NearbyDriversRequestDTO;
+import com.example.Uber.services.implementations.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/location")
+@RequiredArgsConstructor
+public class LocationController {
+    private final LocationService locationService;
+
+    @PostMapping("/driverLocation")
+    public ResponseEntity<Boolean> saveDriverLocation(@RequestBody DriverLocationDTO driverLocation) {
+        Boolean saved = locationService.saveDriverLocation(driverLocation.getDriverId(),
+                driverLocation.getLatitude(),
+                driverLocation.getLongitude());
+        return ResponseEntity.ok(saved);
+    }
+
+    @PostMapping("/nearbyDrivers")
+    public ResponseEntity<List<DriverLocationDTO>> getNearbyDrivers(@RequestBody NearbyDriversRequestDTO request) {
+        List<DriverLocationDTO> nearbyDrivers = locationService.getNearbyDrivers(request.getLatitude(),
+                request.getLongitude(),
+                request.getRadius());
+        return ResponseEntity.ok(nearbyDrivers);
+    }
+}
